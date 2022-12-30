14a92fd4cec6d1bf6b10a6304ca49a52b0ace10f
kayque-willy@hotmail.com <kayque-willy@hotmail.com>
2022-12-30 14:59:44 -0300

    Criação do controlador

--- app_raw.py
+++ app_raw.py
@@ -1,47 +1,23 @@
 from flask import Flask, make_response, jsonify, request
-from python_sql_alchemy.repository.filmes_repository import FilmeRepository
-from python_sql_alchemy.repository.atores_repository import AtoresRepository
+from controllers.filmes_controller import FilmeController
 
-# ------------------------- Repositórios -------------------------
-filme_repository = FilmeRepository()
-atores_repository = AtoresRepository()
+# ------------------------- Controladores -------------------------
+filme_controller = FilmeController()
 
 # ----------------------------- Flask -----------------------------
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
 
 # ------------------- Rotas e métodos da API -------------------
-# ------------------------ FILMES [GET] ------------------------
+# FILMES [GET]
 @app.route("/cinema/filmes", methods=["GET"])
 def get_all_filmes():
-    data = filme_repository.select_all()
-    print(data)
-    filmes = list()
-    for filme in data:
-        filmes.append({
-                "título": filme.titulo, 
-                "gênero": filme.genero, 
-                "ano": filme.ano
-        })
+    return filme_controller.get_all_filmes()
 
-    return make_response(
-        jsonify(
-            mensagem="Lista de filmes", 
-            filmes=filmes
-        )
-    )
-
-# ------------------------ FILMES [POST] ------------------------
+# FILMES [POST]
 @app.route("/cinema/filmes", methods=["POST"])
 def create_filme():
-    filme = request.json
-    filme_repository.insert(filme['titulo'], filme['genero'], filme['ano'])
-    return make_response(
-        jsonify(
-            mensagem='Novo filme adicionado com sucesso!',
-            filme=filme
-        )
-    )
+    return filme_controller.create_filme(request)
 
 # ----------------------------- Executa o Flask -----------------------------
 app.run()
